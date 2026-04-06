8d9b805a3f364dce5844e98413caef45f045c015
sania parvej <sparvej@rrc.ca>
2026-04-05 21:50:22 -0500

    feat: add update and delete handlers in restaurant controller

--- src/api/v1/controllers/restaurant.controller.ts
+++ src/api/v1/controllers/restaurant.controller.ts
@@ -12,3 +12,25 @@ export const createRestaurant = (req: Request, res: Response) => {
   const data = service.addRestaurant(req.body);
   res.status(201).json(data);
 };
+
+// PUT /restaurants/:id
+export const updateRestaurant = (req: Request, res: Response) => {
+  const result = service.updateRestaurant(req.params.id as string, req.body);
+
+  if (!result) {
+    return res.status(404).json({ message: "Restaurant not found" });
+  }
+
+  res.status(200).json(result);
+};
+
+// DELETE /restaurants/:id
+export const deleteRestaurant = (req: Request, res: Response) => {
+  const success = service.deleteRestaurant(req.params.id as string);
+
+  if (!success) {
+    return res.status(404).json({ message: "Restaurant not found" });
+  }
+
+  res.status(200).json({ message: "Deleted successfully" });
+};
