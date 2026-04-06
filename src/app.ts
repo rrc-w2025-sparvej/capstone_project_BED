39cd4955d5505373f4efe806093f889870a0a2ce
sania parvej <sparvej@rrc.ca>
2026-04-05 21:20:13 -0500

    feat: connect restaurant routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import restaurantRoutes from "./api/v1/routes/restaurant.routes";
+
 
 const app = express();
 app.use(express.json());
+app.use("/restaurants", restaurantRoutes);
 
 app.get("/", (req, res) => {
   res.send("Food Ordering API running successfully!");
